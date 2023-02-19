1104e31a631d52d180f47983918997615221f306
Michael Hladky <michael@hladky.at>
2023-02-19 22:55:19 +0100

    refactor: remove source map

---
+++ ts-main.ts
@@ -0,0 +1,25 @@
+import {writeFileSync} from "fs";
+import puppeteer from 'puppeteer';
+import {startFlow} from 'lighthouse';
+
+(async function() {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    const flow = await startFlow(page);
+
+    // Navigate with a URL
+    await flow.navigate('https://example.com');
+
+    // Interaction-initiated navigation via a callback function
+    await flow.navigate(async () => {
+        await page.click('a');
+    });
+
+    // Navigate with startNavigation/endNavigation
+    await flow.startNavigation();
+    await page.click('a');
+    await flow.endNavigation();
+
+    await browser.close();
+    writeFileSync('./tmp/report.html', await flow.generateReport());
+})();
